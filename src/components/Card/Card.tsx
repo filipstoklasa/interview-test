b8bfcfa5f36016a25bbfcce915774d597a97a065
PWCGLB\fstoklasa001 <filip.stoklasa@pwc.com>
2022-06-04 23:23:51 +0200

    UI refactoring

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,45 +1,78 @@
-import { MouseEvent } from 'react';
+import { useState, useCallback, MouseEvent } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 import CardActionArea from '@mui/material/CardActionArea';
 import Image from 'next/image'
+import Collapse from '@mui/material/Collapse'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 type CardComponentProps = {
-	title: string;
 	id: string | number;
+	title: string;
+	releaseYear: number;
 	onClick: (arg: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => void
 	image?: string;
 	perex?: string;
 }
 
-const CardComponent = ({ id, title, perex, image, onClick }: CardComponentProps) => (
-	<Card sx={{ maxWidth: 345 }}>
-		<CardActionArea onClick={onClick} data-identificator={id}>
-			{image && (
-				<Box style={{ position: "relative", height: 140 }}>
-					<Image
-						src={image}
-						layout='fill'
-						alt="card-image"
-						objectFit='cover'
-						objectPosition="left"
-					/>
-				</Box>
-			)}
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="div">
-					{title}
-				</Typography>
-				{perex && (
-					<Typography variant="body2" color="text.secondary">
-						{perex}
-					</Typography>
+const CardComponent = ({ id, title, releaseYear, perex, image, onClick }: CardComponentProps) => {
+	const [expanded, setExpanded] = useState(false)
+
+	const toggleExpander = useCallback(
+		(e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+			e.stopPropagation()
+			setExpanded((prev) => !prev)
+		},
+		[],
+	)
+
+
+	return (
+		<Card sx={{ maxWidth: 340 }}>
+			<CardActionArea onClick={onClick} data-identificator={id}>
+				{image && (
+					<Box style={{ position: "relative", height: 300 }}>
+						<Image
+							src={image}
+							layout='fill'
+							alt="card-image"
+							objectFit='contain'
+							objectPosition="center"
+						/>
+					</Box>
 				)}
-			</CardContent>
-		</CardActionArea>
-	</Card>
-)
+
+				<CardContent>
+					<Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between">
+						<Tooltip title={title}>
+							<Typography gutterBottom variant="subtitle1" noWrap >
+								{title}
+							</Typography>
+						</Tooltip>
+						{perex && (
+							<IconButton onClick={toggleExpander} sx={{ transform: !expanded ? 'rotate(0deg)' : 'rotate(180deg)' }} >
+								<ExpandMoreIcon />
+							</IconButton>
+						)}
+					</Box>
+					<Typography gutterBottom variant="subtitle2" noWrap >
+						{releaseYear}
+					</Typography>
+					{perex && (
+						<Collapse in={expanded}>
+							<Typography variant="body2" color="text.secondary">
+								{perex}
+							</Typography>
+						</Collapse>
+					)}
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	)
+}
 
 export default CardComponent
